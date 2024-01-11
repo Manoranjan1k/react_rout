61bfb12723b0ae1e3ad7ad3e2b102a0eda215afb
Manoranjan1k <manasj790@gmail.com>
2024-01-11 22:53:56 +0530

    changes in Footer

--- React_rout/src/components/Footer/Footer.jsx
+++ React_rout/src/components/Footer/Footer.jsx
@@ -70,8 +70,8 @@ export default function Footer() {
                 <div className="sm:flex sm:items-center sm:justify-between">
                     <span className="text-sm text-gray-500 sm:text-center">
                         © 2023
-                        <a href="https://hiteshchoudhary.com/" className="hover:underline">
-                            hiteshchoudhary
+                        <a href="https://github.com/Manoranjan1k" className="hover:underline">
+                            Manoranjanjha
                         </a>
                         . All Rights Reserved.
                     </span>
